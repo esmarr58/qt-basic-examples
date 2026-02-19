3b1b64c673c1ec141b98c1695f4188163e12ce4c
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-02-18 20:22:23 -0600

    Create bibliotecas.h

---
+++ 2026A/gists/bibliotecas.h
@@ -0,0 +1,8 @@
+//Paso # 6. Agregar bibliotecas para opencv
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+//Paso #12 Uso de qtimer
+#include<QTimer>
