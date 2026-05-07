6be5738300eb0a2b36fdf9afdcca3e561e1c0627
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-05-07 07:40:58 -0600

    Create servo.ino

---
+++ 2026A/Practicas/Practica4/servo.ino
@@ -0,0 +1,24 @@
+#include <ESP32Servo.h>
+
+Servo miServo;        // Crear objeto tipo Servo
+const int pinServo = 35;  // Pin donde está conectado el servo
+int angulo = 0;         // Ángulo inicial
+
+
+void setup() {
+  // put your setup code here, to run once:
+   miServo.setPeriodHertz(50);          // Frecuencia típica de los servos
+   miServo.attach(pinServo, 500, 2400); // Pin, pulso mínimo y máximo en microsegundos
+   miServo.write(angulo);  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int angulo = 0; angulo <= 180; angulo +=10){
+       miServo.write(angulo);
+       delay(300);
+    
+  }
+
+}
